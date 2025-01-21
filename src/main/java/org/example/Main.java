1a7c03e161564ec60bda828dc2a1773b78f3b845
joa <joaquin_trujillo@icloud.com>
2025-01-22 00:16:09 +0100

    feat: example query from database

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.example.models.Database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String sql = "SELECT * FROM jugadores";
+
+        try (Connection connection = Database.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(sql);) {
+            while(rs.next()) {
+                System.out.println("player");
+                System.out.println("id: " + rs.getInt(1));
+                System.out.println("name: " + rs.getString(2));
+                System.out.println("position: " + rs.getString(3));
+                System.out.println("team id: " + rs.getInt(4));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
