bd53c45099f482eec0d6e8b0d1f9296eb8357ef3
joa <joaquin_trujillo@icloud.com>
2025-01-22 13:56:17 +0100

    todo: fix custom ui

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.example.lib.ModernTheme;
 import org.example.models.Database;
 
+import javax.swing.*;
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
+        ModernTheme.apply();
+
         String sql = "SELECT * FROM jugadores";
 
         try (Connection connection = Database.getConnection();
@@ -23,5 +28,26 @@ public class Main {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+
+        JLabel label = new JLabel("testing");
+        JButton button = new JButton("button test");
+        JPanel panel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        panel.add(label, gbc);
+        panel.add(button, gbc);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+        SwingUtilities.updateComponentTreeUI(frame);
     }
 }
