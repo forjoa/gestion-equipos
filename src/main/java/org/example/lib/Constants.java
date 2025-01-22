0a3157d5bb9db8f0c2eb03934340d2a2bed4afbf
joa <joaquin_trujillo@icloud.com>
2025-01-22 16:40:59 +0100

    build: queries for teams

--- src/main/java/org/example/lib/Constants.java
+++ src/main/java/org/example/lib/Constants.java
@@ -1,4 +1,10 @@
 package org.example.lib;
 
 public class Constants {
+    // teams queries
+    public static final String INSERT_TEAM = "INSERT INTO equipos (nombre, ciudad, estadio) VALUES (?, ?, ?)";
+    public static final String GET_TEAMS = "SELECT * FROM equipos";
+    public static final String TEAM_BY_ID = "SELECT * FROM equipos WHERE id = ?";
+    public static final String UPDATE_TEAM = "UPDATE equipos SET nombre = ?, ciudad = ?, estadio = ? WHERE id = ?";
+    public static final String DELETE_TEAM = "DELETE FROM equipos WHERE id = ?";
 }
