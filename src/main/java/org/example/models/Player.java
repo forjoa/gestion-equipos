360861df2290415f34703c4251f256e9e78acf29
joa <joaquin_trujillo@icloud.com>
2025-01-30 18:31:51 +0100

    feat: replace getters and setters w lombok

--- src/main/java/org/example/models/Player.java
+++ src/main/java/org/example/models/Player.java
@@ -1,50 +1,17 @@
 package org.example.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Player {
     private Integer id;
     private String name;
     private String position;
     private Integer team_id;
-
-    public Player(Integer id, String name, String position, Integer team_id) {
-        this.id = id;
-        this.name = name;
-        this.position = position;
-        this.team_id = team_id;
-    }
-
-    public Player() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public Integer getTeam_id() {
-        return team_id;
-    }
-
-    public void setTeam_id(Integer team_id) {
-        this.team_id = team_id;
-    }
 }
--- src/main/java/org/example/models/Team.java
+++ src/main/java/org/example/models/Team.java
@@ -1,50 +1,17 @@
 package org.example.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Team {
     private Integer id;
     private String name;
     private String city;
     private String stadium;
-
-    public Team(Integer id, String name, String city, String stadium) {
-        this.id = id;
-        this.name = name;
-        this.city = city;
-        this.stadium = stadium;
-    }
-
-    public Team() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStadium() {
-        return stadium;
-    }
-
-    public void setStadium(String stadium) {
-        this.stadium = stadium;
-    }
 }
